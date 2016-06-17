7b6cdfea02e58782ef144fb7c4b77e076064bfc8
Blake Strickland <Stricklandb02@gmail.com>
2016-06-16 22:41:52 -0400

    Node IO

--- read.js
+++ read.js
@@ -1,8 +1,26 @@
+
+/*get file system*/
 var fs = require('fs')
-var buffer = fs.readFileSync("read.js")
+/*grab the 3rd process*/
+var file = process.argv[2]
+/*read file using utf8*/
+var fileBuffer = fs.readFileSync(file, 'utf8')
+/*remove false linebreaks?*/
+var fileBuffer = fileBuffer.replace(/\n+$/, '');
+/*create buffer (array) split on new line characters*/
+fileBuffer = fileBuffer.split('\n');
+/*subtract 1 for line breaks not lines*/
+lineCount = fileBuffer.length-1;
 
-var string = buffer.toString()
+console.log(lineCount);
+// var buffer = fs.readFileSync("readfile.txt")
 
-var array = string.split("\n")
-
-console.log((array.length))
+// var string = buffer.toString()
+//
+// var array = string.split("\n")
+//
+// console.log((array.length - 1))
+// console.log(string)
+// console.log(array)
+// console.log(file)
+// console.log(fileBuffer)
